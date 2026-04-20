e130d7fd5c17e4f03cfc0782da1c087ed4ceb42a
theresasogunle <sogunledolapo@gmail.com>
2026-04-20 07:34:14 +0100

    fix settings screen

--- app/dashboard/(tabs)/settings.tsx
+++ app/dashboard/(tabs)/settings.tsx
@@ -1,5 +1,8 @@
 import SettingsScreen from "@/components/settings/SettingsScreen";
+import { useSettingsSync } from "@/stores/settingsStore";
 
 export default function SettingsTab() {
+  useSettingsSync();
+
   return <SettingsScreen showBackButton={false} />;
 }
